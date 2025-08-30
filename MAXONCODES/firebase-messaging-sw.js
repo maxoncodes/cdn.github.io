a1f3cde8a0d0e476b6f05420b55bc725f67692ad
MAXON <maxonofficial1@gmail.com>
2025-08-30 21:35:30 +0530

    Add files via upload

---
+++ MAXONCODES/firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+﻿// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.1/firebase-messaging.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyARNVArmUXwSqZI8w7YDx6YaIT-_LZ_Z0g",
+  authDomain: "maxoncodes-44d8d.firebaseapp.com",
+  projectId: "maxoncodes-44d8d",
+  storageBucket: "maxoncodes-44d8d.appspot.com",
+  messagingSenderId: "423151629268",
+  appId: "1:423151629268:web:9dab6dac5406fb058ddc41"
+});
+
+const messaging = firebase.messaging();
